74d71db10639627c7051670647269a0b894a3170
Max Bergdahl <maxbergdahl@gmail.com>
2015-09-07 14:22:02 +0200

    New folders, no change to code.

---
+++ workspace/ProgPLab1/ProgPLab1.hs
@@ -0,0 +1,14 @@
+-- Max Bergdahl och Niklas Bergdahl 7/9-15
+module
+F1
+where
+-- Vad ska de andra funktionernas typsignaturer vara?
+fib ::
+Integer
+->
+Integer
+fib n = 0
+rovarsprak s = s
+karpsravor s = s
+medellangd s = 1.0
+skyffla s = s
