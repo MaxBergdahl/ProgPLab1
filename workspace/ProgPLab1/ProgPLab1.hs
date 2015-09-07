3214f0f9fa40855c6f0a9772e9f14f4cc7898293
Max Bergdahl <maxbergdahl@gmail.com>
2015-09-07 16:09:05 +0200

    Added code to function fib. Receive: parse error on inout `else' when attempting to load the module into ghci.

--- workspace/ProgPLab1/ProgPLab1.hs
+++ workspace/ProgPLab1/ProgPLab1.hs
@@ -1,13 +1,14 @@
--- Max Bergdahl och Niklas Bergdahl 7/9-15
+-- Max Bergdahl och Niklas Bergdahl 7/9-15 --
 module
 F1
 where
 -- Vad ska de andra funktionernas typsignaturer vara?
-fib ::
-Integer
-->
-Integer
-fib n = 0
+fib :: Integer -> Integer
+fib n = if n == 0
+	then let x = 0
+	else if n == 1
+		then let x = 1
+		else fib(n-1) + (fib(n-2))
 rovarsprak s = s
 karpsravor s = s
 medellangd s = 1.0
