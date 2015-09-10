78d9849c9feda338a9d44e0cdc74d37f4542e47d
Max Bergdahl <maxbergdahl@gmail.com>
2015-09-10 14:52:36 +0200

    Updated 'ProgPLab1.hs' function fib works.

--- github/ProgPLab1/ProgPLab1.hs
+++ github/ProgPLab1/ProgPLab1.hs
@@ -4,11 +4,11 @@ F1
 where
 -- Vad ska de andra funktionernas typsignaturer vara?
 fib :: Integer -> Integer
-fib n = if n == 0
-	then let x = 0
-	else if n == 1
-		then let x = 1
-		else fib(n-1) + (fib(n-2))
+fib 0 = 0
+fib 1 = 1
+fib n = if n > 1
+	then fib (n-1) + fib (n-2)
+	else n
 rovarsprak s = s
 karpsravor s = s
 medellangd s = 1.0
