95759900849f15e3180badd3df54108e137f32da
Max Bergdahl <maxbergdahl@gmail.com>
2015-09-11 16:28:00 +0200

    Removed useless parentheses.

--- github/ProgPLab1/ProgPLab1.hs
+++ github/ProgPLab1/ProgPLab1.hs
@@ -16,15 +16,15 @@ fib n = if n > 1
 rovarsprak :: String -> String
 --rovarsprak skriven av Max Bergdahl
 rovarsprak (s:b)
-	| not(elem s "aeiouy") = s:'o':s : rovarsprak(b)
-	| otherwise = s:rovarsprak(b)
+	| not(elem s "aeiouy") = s:'o':s : rovarsprak b
+	| otherwise = s:rovarsprak b
 rovarsprak s = s
 -- a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z.
 --karpsravor skriven av Max Bergdahl
 karpsravor :: String -> String
 karpsravor (s:b)
 	| not(elem s "aeiouy") = s:karpsravor(drop 2 b)
-	| otherwise = s:karpsravor(b)
+	| otherwise = s:karpsravor b
 karpsravor s = s
 --karpsdrop s = drop 2 s;
 medellangd s = 1.0
