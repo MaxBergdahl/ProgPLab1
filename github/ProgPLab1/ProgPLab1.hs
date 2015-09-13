c80a5ce577d48a7c8356056b1db52c46d1ffa733
Max Bergdahl <maxbergdahl@gmail.com>
2015-09-13 18:31:25 +0200

    Get error "not in scope: data constructor double" in function medellangd.

--- github/ProgPLab1/ProgPLab1.hs
+++ github/ProgPLab1/ProgPLab1.hs
@@ -1,31 +1,38 @@
 -- Max Bergdahl och Niklas Bergdahl 7/9-15 --
-module
-F1
-where
+module F1 where
+import Data.Char
+
 -- Vad ska de andra funktionernas typsignaturer vara?
-fib :: Integer -> Integer
 -- fib skriven av Max Bergdahl
+fib :: Integer -> Integer
 fib 0 = 0
 fib 1 = 1
 fib n = if n > 1
 	then fib (n-1) + fib (n-2)
 	else n
 
---rovarsprak s
-
-rovarsprak :: String -> String
 --rovarsprak skriven av Max Bergdahl
+rovarsprak :: String -> String
 rovarsprak (s:b)
 	| not(elem s "aeiouy") = s:'o':s : rovarsprak b
 	| otherwise = s:rovarsprak b
 rovarsprak s = s
 -- a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z.
+
 --karpsravor skriven av Max Bergdahl
 karpsravor :: String -> String
 karpsravor (s:b)
 	| not(elem s "aeiouy") = s:karpsravor(drop 2 b)
 	| otherwise = s:karpsravor b
 karpsravor s = s
---karpsdrop s = drop 2 s;
-medellangd s = 1.0
+
+Double totalSize = 0
+Double wordCount = 1
+--medellangd skriven av Max Bergdahl och Niklas Bergdahl
+medellangd :: String -> Double
+medellangd (s:b)
+	| s isAlpha = succ totalSize medellangd b
+	| elem s " " = succ wordCount medellangd b
+medellangd s = totalSize `div` wordCount
+
 skyffla s = s
